2fdb95c6f8ba2743bb366204e3dff9ea8cfde4c3
Creatorpiyush <piyushanand.1221@gmail.com>
2020-07-18 18:35:48 +0530

    assingment1

---
+++ assignment_1/ques1.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+/**
+ * ques1
+ */
+public class ques1 {
+
+    public static void main(String[] args) {
+
+        Scanner s = new Scanner(System.in);
+
+        int n = s.nextInt();
+        int k = 0, count = 0, count1 = 0;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print("  ");
+                count++;
+            }
+
+            while (k != 2 * i - 1) {
+                if (count <= n - 1) {
+                    System.out.print((i + k) + " ");
+                    count++;
+                } else {
+                    count1++;
+                    System.out.print((i + k - 2 * count1) + " ");
+                }
+
+                ++k;
+            }
+            count1 = count = k = 0;
+            System.out.println();
+        }
+    }
+
+}
---
+++ assignment_1/ques2.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+/**
+ * ques2
+ */
+public class ques2 {
+
+    public static void main(String[] args) {
+
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+
+        int reversed = 0;
+
+        while (n != 0) {
+            int digit = n % 10;
+            reversed = reversed * 10 + digit;
+            n /= 10;
+        }
+
+        System.out.println(reversed);
+
+    }
+}
