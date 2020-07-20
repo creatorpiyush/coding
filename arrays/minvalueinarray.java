8091bfe314f7af914d7a9f5b18a33f7a86107149
Creatorpiyush <piyushanand.1221@gmail.com>
2020-07-20 20:37:19 +0530

    Minimum value in a Array

---
+++ arrays/minvalueinarray.java
@@ -0,0 +1,36 @@
+package arrays;
+
+import java.util.*;
+
+public class minvalueinarray {
+
+    public static void main(String[] args) {
+
+        Scanner s = new Scanner(System.in);
+
+        int n = s.nextInt();
+
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = s.nextInt();
+        }
+
+        System.out.println(minvalue(arr));
+
+    }
+
+    public static int minvalue(int[] arr) {
+
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < min) {
+                min = arr[i];
+            }
+
+        }
+
+        return min;
+
+    }
+
+}
