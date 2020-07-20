e81c7a3f3bd63c0e2da5013601f9b84af75b2017
Creatorpiyush <piyushanand.1221@gmail.com>
2020-07-20 20:30:24 +0530

    Maximum value inside a Array

---
+++ arrays/maxvalue.java
@@ -0,0 +1,31 @@
+package arrays;
+
+import java.util.Scanner;
+
+public class maxvalue {
+
+    public static void main(String[] args) {
+
+        Scanner s = new Scanner(System.in);
+
+        int n = s.nextInt();
+
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = s.nextInt();
+        }
+
+        System.out.println(maxvalue(arr));
+    }
+
+    public static int maxvalue(int[] arr) {
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+        return max;
+    }
+
+}
