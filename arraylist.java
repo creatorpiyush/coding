5b8d6aadb0a9220c653ec56120efdff10b6b720f
Creatorpiyush <piyushanand.1221@gmail.com>
2020-07-21 22:49:15 +0530

    ArratList => no boundation of length

---
+++ arraylist.java
@@ -0,0 +1,50 @@
+import java.util.ArrayList;
+
+/**
+ * arraylist
+ */
+public class arraylist {
+
+    public static void main(String[] args) {
+
+        ArrayList<Integer> list = new ArrayList<>();
+
+        System.out.println(list); // return's empty list
+
+        System.out.println(list.toString());
+
+        list.add(10);
+        System.out.println(list);
+        System.out.println(list.size());
+
+        list.add(20);
+        System.out.println(list);
+        System.out.println(list.size());
+
+        list.add(30);
+        System.out.println(list);
+        System.out.println(list.size());
+
+        list.add(40);
+        System.out.println(list);
+        System.out.println(list.size());
+
+        // for (int i = 0; i < 100; i++) {
+        // list.add(i);
+        // }
+        // System.out.println(list);
+
+        list.set(2, 100);
+        System.out.println(list);
+
+        list.remove(1);
+        System.out.println(list);
+
+        System.out.println(list.get(2));
+
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println(list.get(i));
+        }
+    }
+
+}
