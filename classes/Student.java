d3bb8165f72c8855f260c5996d0459e7c4fb686e
Creatorpiyush <piyushanand.1221@gmail.com>
2020-07-28 19:42:57 +0530

    static and final keyword usage

--- classes/Student.java
+++ classes/Student.java
@@ -34,4 +34,37 @@ public class Student {
 
     }
 
+    // * final means value can be set only once. Can be public
+    public final int rollNo;
+
+    // * static means, it belongs to the class(it is not copied in each object)
+    private static int NumOfStudents = 0;
+
+    public static int GetNumOfStudents() {
+        return Student.NumOfStudents;
+    }
+
+    private static String UniformColor = "White";
+
+    public static String GetUniformColor() {
+        return Student.UniformColor;
+    }
+
+    public static void SetUniformColor(String uniformColor) {
+        Student.UniformColor = uniformColor;
+    }
+
+    // * nothing more than the effects of both static and final
+    public static final int MAX_STUDENTS = 1000;
+
+    public Student(String name) throws Exception {
+        if (Student.NumOfStudents == Student.MAX_STUDENTS) {
+            throw new Exception("Max limit reached...");
+        }
+        this.setName(name);
+
+        Student.NumOfStudents++;
+        this.rollNo = Student.NumOfStudents;
+    }
+
 }
--- classes/StudentClient.java
+++ classes/StudentClient.java
@@ -4,17 +4,36 @@ public class StudentClient {
 
     public static void main(String[] args) throws Exception {
 
-        Student s = new Student();
+        // * normal object creation
+        // Student s = new Student();
 
-        s.setName("Piyush");
-        s.setAge(19);
-        System.out.println(s.getName());
-        System.out.println(s.getAge());
+        // s.setName("Piyush");
+        // s.setAge(19);
+        // System.out.println(s.getName());
+        // System.out.println(s.getAge());
 
-        s.setName("Piyush");
-        s.setAge(-10);
-        System.out.println(s.getName());
-        System.out.println(s.getAge());
+        // s.setName("Piyush");
+        // s.setAge(-10);
+        // System.out.println(s.getName());
+        // System.out.println(s.getAge());
+
+        Student[] students = new Student[1000];
+        // System.out.println(students[0].rollNo);
+
+        for (int i = 0; i < students.length; i++) {
+            students[i] = new Student(i + "th");
+            System.out.println(Student.GetNumOfStudents());
+        }
+
+        // Able to access static field but cant change the value
+        System.out.println(Student.MAX_STUDENTS);
+        // Student.MAX_STUDENTS = 2000; // ! error can't change value
+
+        System.out.println(students[250].rollNo);
+
+        System.out.println(students[250].getName());
+        students[250].setName("ABC");
+        System.out.println(students[250].getName());
 
     }
 
