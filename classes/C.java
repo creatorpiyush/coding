0aad07e8e26085a406e0080b892a32d21efd0550
Creatorpiyush <piyushanand.1221@gmail.com>
2020-07-28 22:00:48 +0530

    @Overriding different functions

---
+++ classes/C.java
@@ -0,0 +1,29 @@
+package classes;
+
+public class C extends P {
+
+    int d2 = 21;
+    int d = 22;
+
+    public static int NUMBER = 20;
+
+    public void Fun2() {
+        System.out.println("Inside C's Fun2");
+    }
+
+    public void Fun() {
+        System.out.println("Inside C's Fun");
+    }
+
+    // ! can't override final function (final in class P)
+    // @Override
+    // public void finalFun() {
+    // System.out.println("Inside C's finalFun");
+    // }
+
+    public static void staticFun() {
+        System.out.println("Inside C's staticFun");
+
+    }
+
+}
---
+++ classes/P.java
@@ -0,0 +1,27 @@
+package classes;
+
+public class P {
+
+    int d1 = 11;
+    int d = 12;
+
+    public static int NUMBER = 10;
+
+    public void Fun1() {
+        System.out.println("Inside P's Fun1...");
+    }
+
+    public void Fun() {
+        System.out.println("Inside P's Fun...");
+    }
+
+    // final function can't be overridden
+    public final void finalFun() {
+        System.out.println("Inside P's finalFun...");
+    }
+
+    public static void staticFun() {
+        System.out.println("Inside P's staticFun...");
+    }
+
+}
