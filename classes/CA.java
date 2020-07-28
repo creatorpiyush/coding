720707be93ad3637cc342fba26d7a43b8f437fca
Creatorpiyush <piyushanand.1221@gmail.com>
2020-07-28 22:07:06 +0530

    abstract class extention

---
+++ classes/CA.java
@@ -0,0 +1,13 @@
+package classes;
+
+public class CA extends PA {
+
+    public void Fun() {
+        System.out.println("Inside CA's Fun");
+    }
+
+    public void Fun2() {
+        System.out.println("Inside CA's Fun2");
+    }
+
+}
---
+++ classes/PA.java
@@ -0,0 +1,17 @@
+package classes;
+
+public abstract class PA {
+
+    public int num = 10;
+
+    public void Fun1() {
+        System.out.println("Inside PA's Fun1");
+    }
+
+    public abstract void Fun();
+
+    public void Fun2() {
+
+    }
+
+}
