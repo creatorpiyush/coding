cf25914fd5b716a7e6d8f485ef5d1c5039379a64
Creatorpiyush <piyushanand.1221@gmail.com>
2020-07-30 21:44:47 +0530

    queue client

--- queue/quaclient.java
+++ queue/quaclient.java
@@ -1,15 +1,33 @@
 package queue;
 
+import java.util.Scanner;
+
 public class quaclient {
 
     public static void main(String[] args) throws Exception {
 
-        queueusingarray queue = new queueusingarray();
+        Scanner s = new Scanner(System.in);
 
-        System.out.println(queue.size());
+        int n = s.nextInt();
 
-        queue.enqueue(10);
+        queueusingarray queue = new queueusingarray(n);
+
+        // System.out.println(queue.size());
+
+        for (int i = 0; i < n; i++) {
+            queue.enqueue(s.nextInt());
+        }
+
+        // queue.enqueue(10);
         queue.display();
+
+        System.out.println("***********");
+        System.out.println(queue.dequque());
+        queue.display();
+
+        System.out.println("***********");
+        System.out.println(queue.front());
+
     }
 
 }
