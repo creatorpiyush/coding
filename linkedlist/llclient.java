df9fff9530fab50910ff73e255df4cf706be5136
Creatorpiyush <piyushanand.1221@gmail.com>
2020-08-01 19:56:06 +0530

    linked list client

---
+++ linkedlist/llclient.java
@@ -0,0 +1,13 @@
+package linkedlist;
+
+public class llclient {
+
+    public static void main(String[] args) {
+
+        linkedlist ll = new linkedlist();
+
+        ll.addFirst(10);
+
+    }
+
+}
