dcd9bfc882162dbb715d18f7b40495a69588d3bc
Creatorpiyush <piyushanand.1221@gmail.com>
2020-08-04 13:36:44 +0530

    Binary Tree taking input

---
+++ binarytree/BinaryTree.java
@@ -0,0 +1,61 @@
+package binarytree;
+
+import java.util.Scanner;
+
+/**
+ * binarytree
+ */
+public class BinaryTree {
+
+    private class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data, Node left, Node right) {
+            this.data = data;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    private Node root;
+    private int size;
+
+    public BinaryTree() {
+        Scanner s = new Scanner(System.in);
+        this.root = this.takeInpuNode(s, null, false);
+    }
+
+    public Node takeInpuNode(Scanner s, Node parent, boolean isLeftOrRight) {
+
+        if (parent == null) {
+            System.out.println("Enter the data for the root node :- ");
+        } else {
+            if (isLeftOrRight) {
+                System.out.println("Enter the data for the left child of " + parent.data);
+            } else {
+                System.out.println("Enter the data for the right child of " + parent.data);
+            }
+        }
+
+        int data = s.nextInt();
+        Node node = new Node(data, null, null);
+        this.size++;
+
+        System.out.println("Do you have left child of " + node.data);
+        boolean choice = s.nextBoolean();
+        if (choice) {
+            node.left = this.takeInpuNode(s, node, true);
+        }
+
+        System.out.println("Do you have right child of " + node.data);
+        choice = s.nextBoolean();
+        if (choice) {
+            node.left = this.takeInpuNode(s, node, false);
+        }
+
+        return node;
+    }
+
+}
---
+++ binarytree/btclient.java
@@ -0,0 +1,11 @@
+package binarytree;
+
+public class btclient {
+
+    public static void main(String[] args) {
+
+        BinaryTree tree = new BinaryTree();
+
+    }
+
+}
