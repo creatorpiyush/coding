242cfdf814dcaccdba0cee95eb1da39b5152a21b
creatorpiyush <piyushanand.1221@gmail.com>
2020-08-20 01:06:36 +0530

    hashtable

--- hashtable/hashtable.java
+++ hashtable/hashtable.java
@@ -1,17 +1,21 @@
 package hashtable;
 
+/**
+ * hashtable
+ */
 public class hashtable<K, V> {
-    private class HTPairs {
+
+    private class HTPair {
         K key;
         V value;
 
-        public HTPairs(K key, V value) {
+        public HTPair(K key, V value) {
             this.key = key;
             this.value = value;
         }
 
         public boolean equals(Object obj) {
-            HTPairs op = (HTPairs) obj;
+            HTPair op = (HTPair) obj;
             return this.key.equals(op.key);
         }
 
@@ -21,7 +25,7 @@ public class hashtable<K, V> {
     }
 
     public static final int DEFAULT_CAPACITY = 10;
-    private linkedlist<HTPairs>[] bucketArray;
+    private linkedlist<HTPair>[] bucketarray;
     private int size;
 
     public hashtable() {
@@ -29,29 +33,104 @@ public class hashtable<K, V> {
     }
 
     public hashtable(int capacity) {
-        this.bucketArray = (linkedlist<HTPairs>[]) new linkedlist[capacity];
+        this.bucketarray = (linkedlist<HTPair>[]) new linkedlist[capacity];
         this.size = 0;
     }
 
     public void put(K key, V value) throws Exception {
-
         int bi = this.hashFunction(key);
-        linkedlist<HTPairs> bucket = this.bucketArray[bi];
-        HTPairs pta = new HTPairs(key, value);
+        linkedlist<HTPair> bucket = this.bucketarray[bi];
+        HTPair pta = new HTPair(key, value);
         if (bucket == null) {
-            bucket = new LinkedList<>();
+            bucket = new linkedlist<>();
             bucket.addLast(pta);
             this.size++;
-            this.bucketArray[bi] = bucket;
-        } else{
+            this.bucketarray[bi] = bucket;
+        } else {
             int foundAt = bucket.find(pta);
+            if (foundAt == -1) {
+                bucket.addLast(pta);
+                this.size++;
+            } else {
+                HTPair pair = bucket.getAt(foundAt);
+                pair.value = value;
+            }
+        }
+
+        double lamda = (this.size * 1.0) / this.bucketarray.length;
+        if (lamda > 0.75) {
+            this.rehash();
+        }
+    }
+
+    public String toString() {
+        String str = "";
+        for (linkedlist<HTPair> bucket : this.bucketarray) {
+            if (bucket != null && !bucket.isEmpty()) {
+                str = str + bucket;
+            } else {
+                str = str + "Null";
+            }
+            str = str + "\n";
+            str = str + "~~~~~~~~~~~~~~~~~~~~~~~~~~~\n";
+        }
+        str = str + "\n";
+        str = str + "*******************************";
+        return str;
+    }
+
+    public V get(K key) throws Exception {
+        int bi = this.hashFunction(key);
+        linkedlist<HTPair> bucket = this.bucketarray[bi];
+        HTPair ptf = new HTPair(key, null);
+        if (bucket == null) {
+            return null;
+        } else {
+            int foundAt = bucket.find(ptf);
+            if (foundAt == -1) {
+                return null;
+            } else {
+                HTPair pair = bucket.getAt(foundAt);
+                return pair.value;
+            }
+        }
+    }
+
+    public V remove(K key) throws Exception {
+        int bi = this.hashFunction(key);
+        linkedlist<HTPair> bucket = this.bucketarray[bi];
+        HTPair ptf = new HTPair(key, null);
+        if (bucket == null) {
+            return null;
+        } else {
+            int foundAt = bucket.find(ptf);
+            if (foundAt == -1) {
+                return null;
+            } else {
+                HTPair pair = bucket.getAt(foundAt);
+                bucket.removeAt(foundAt);
+                this.size--;
+                return pair.value;
+            }
         }
     }
 
     private int hashFunction(K key) {
         int hc = key.hashCode();
         hc = Math.abs(hc);
-        int bi = hc % this.bucketArray.length;
+        int bi = hc % this.bucketarray.length;
         return bi;
+    }
+
+    private void rehash() throws Exception {
+        linkedlist<HTPair>[] oba = this.bucketarray;
+        this.bucketarray = (linkedlist<HTPair>[]) new linkedlist[2 * oba.length];
+        this.size = 0;
+        for (linkedlist<HTPair> ob : oba) {
+            while (ob != null && !ob.isEmpty()) {
+                HTPair rp = ob.removeFirst();
+                this.put(rp.key, rp.value);
+            }
+        }
     }
 }
--- hashtable/htclient.java
+++ hashtable/htclient.java
@@ -6,6 +6,20 @@ public class htclient {
         hashtable<String, Integer> table = new hashtable<>(4);
         table.put("India", 300);
         table.put("Russia", 200);
+        System.out.println(table);
+
+        table.put("China", 325);
+        table.put("Finland", 50);
+        table.put("Aus", 100);
+        table.put("NZ", 150);
+        table.put("India", 320);
+        table.put("USA", 155);
+        System.out.println(table);
+        System.out.println(table.get("Russia"));
+        System.out.println(table.get("USA"));
+
+        table.remove("Finland");
+        System.out.println(table);
     }
 
 }
